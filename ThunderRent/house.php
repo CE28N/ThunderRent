1e2510fbe8894e6e865897c5f992e10c561bccf7
Ash Chan <ashchan1122@gmail.com>
2016-03-03 01:05:30 +0800

    Search feature

--- ThunderRent/house.php
+++ ThunderRent/house.php
@@ -30,22 +30,59 @@ checkSession();
 
 		<h2>House</h2>
 		<h4>Welcome back: <b><?php echo $_SESSION['userName']; ?></b></h4>
-		Avgerage price: <b>$<?php echo avgPrice(); ?>/ft²</b>
 
 		<div id="list">
+			<div class="link">Avgerage price: <b>$<?php echo avgPrice(); ?>/ft²</b></div>
+			<div class="search">
+				<form method="post">
+					Search by:
+					<input name="search" type="radio" value="title">Title
+					<input name="search" type="radio" value="district">District
+					<input name="search" type="radio" value="price">Price
+					<input name="query" type="text" value="">
+					<input name="submit" type="submit" value="Search">
+				</form>
+			</div>
 			<?php
+			ob_start();
 			$connection = connectDB();
-			$query = mysqli_query($connection, "SELECT houseID, title, district, size FROM house_profile");
+			$query = mysqli_query($connection, "SELECT houseID, title, district, price FROM house_profile");
 			if (mysqli_num_rows($query) > 0) {
-				echo '<table><tr><th>Title</th><th>District</th><th>Size</th><th>Link</th></tr>';
+				echo '<table><tr><th>Title</th><th>District</th><th>Price</th><th>Link</th></tr>';
 				while($row = mysqli_fetch_assoc($query)) {
-					echo '<tr><td>'.$row['title'].'</td><td>'.$row['district'].'</td><td>'.$row['size'].'</td><td><a href="viewHouse.php?houseID='.$row['houseID'].'">View</a></td></tr>';
+					echo '<tr><td>'.$row['title'].'</td><td>'.$row['district'].'</td><td>'.$row['price'].'</td><td><a href="viewHouse.php?houseID='.$row['houseID'].'">View</a></td></tr>';
 				}
 				echo '</table>';
 
 				echo '<div class="submit"><a href="host.php">Manage Ads</a></div>';
 			} else {
 				echo '0 results';
+			}
+
+			if (isset($_POST['submit'])) {
+				if ($_POST['query'] == NULL) {
+					exit();
+				}
+
+				ob_end_clean();
+				ob_start();
+				$filter = $_POST['search'];
+				$string = $_POST['query'];
+
+				$query = mysqli_query($connection, "SELECT houseID, title, district, price FROM house_profile WHERE ".$filter." LIKE '%".$string."%'");
+				if (mysqli_num_rows($query) > 0) {
+					echo '<table><tr><th>Title</th><th>District</th><th>Price</th><th>Link</th></tr>';
+					while($row = mysqli_fetch_assoc($query)) {
+						echo '<tr><td>'.$row['title'].'</td><td>'.$row['district'].'</td><td>'.$row['price'].'</td><td><a href="viewHouse.php?houseID='.$row['houseID'].'">View</a></td></tr>';
+					}
+					echo '</table>';
+
+					echo '<div class="submit"><a href="host.php">Manage Ads</a></div>';
+				} else {
+					echo '0 results';
+				}
+
+				echo '<div class="link"><a href="">Back to list</a></div>';
 			}
 			?>
 		</div>
